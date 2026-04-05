3da9502e5e8925bb3754a96cb6ff2abe3a4dc873
ISRAEL_dp24 <dolapoosunkoya5@gmail.com>
2026-04-05 18:43:48 -0400

    fixing errors

--- app/src/main/java/com/comp3074_101384549/projectui/ui/adapter/BookingAdapter.kt
+++ app/src/main/java/com/comp3074_101384549/projectui/ui/adapter/BookingAdapter.kt
@@ -3,7 +3,6 @@ package com.comp3074_101384549.projectui.ui.adapter
 import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
