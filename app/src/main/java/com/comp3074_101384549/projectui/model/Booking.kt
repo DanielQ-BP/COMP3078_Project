cdefca11a95bc6c999c60744958199db560b9520
ShadowK4t <saragmo1629@gmail.com>
2026-04-05 23:38:15 -0400

    Fix merge conficts

--- app/src/main/java/com/comp3074_101384549/projectui/model/Booking.kt
+++ app/src/main/java/com/comp3074_101384549/projectui/model/Booking.kt
@@ -11,12 +11,8 @@ data class Booking(
     val referenceCode: String = "",
     val address: String = "",
     val pricePerHour: Double = 0.0,
-<<<<<<< HEAD
     val fineAmount: Double = 0.0,
     val finePaid: Boolean = false
-=======
-    val referenceCode: String = ""
->>>>>>> 7d11e11e8bd61c43c087a6873997832dc73793a3
 ) {
     fun toBookingEntity(): BookingEntity {
         val date = if (startTime.length >= 10) startTime.take(10) else startTime
